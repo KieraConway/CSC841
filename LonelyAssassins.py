f84ebc42f9a55768b726e3fec7f52f05523a3d36
KieraConway <66643785+KieraConway@users.noreply.github.com>
2024-04-11 19:54:18 -0700

    Create LonelyAssassins.py

---
+++ LonelyAssassins.py
@@ -0,0 +1,2 @@
+#simulate malicious file
+Lonely Assassins, that's what they used to be called. No one quite knows where they came from, but they're as old as the Universe, or very nearly. And they have survived this long because they have the most perfect defence system ever evolved. They are Quantum Locked.
